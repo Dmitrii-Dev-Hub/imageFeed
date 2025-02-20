97138452ef1bc8f2f817b7ff77029146e90f2359
Dmitrii-Dev-Hub <a0@MacBook-Air-0.local>
2025-02-20 22:27:53 +0400

    no message

---
+++ imageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imageFeed
+//
+//  Created by 0 on 20/2/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ imageFeedTests/imageFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  imageFeedTests.swift
+//  imageFeedTests
+//
+//  Created by 0 on 20/2/2025.
+//
+
+import Testing
+@testable import imageFeed
+
+struct imageFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
